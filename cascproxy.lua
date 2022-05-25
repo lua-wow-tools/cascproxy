d19d2d3e393f3d5e9fd59259e1d3b93c26a03118
Ferronn <ferronn@ferronn.dev>
2022-05-25 13:09:12 +0000

    make all products available

--- cascproxy.lua
+++ cascproxy.lua
@@ -1,51 +1,55 @@
 local args = (function()
   local parser = require('argparse')()
   parser:option('-c --cache', 'cache directory', 'cache')
-  parser:option('-f --flavor', 'wow flavor'):count(1):choices({ 'mainline', 'tbc', 'vanilla' })
   parser:option('-p --port', 'web server port', 8080)
-  parser:flag('--ptr', 'use ptr build')
   parser:flag('-v --verbose', 'verbose printing')
   return parser:parse()
-end)()
-
-local product = (function()
-  local m = {
-    mainline = { [false] = 'wow', [true] = 'wowt' },
-    tbc = { [false] = 'wow_classic', [true] = 'wow_classic_ptr' },
-    vanilla = { [false] = 'wow_classic_era', [true] = 'wow_classic_era_ptr' },
-  }
-  return m[args.flavor][not not args.ptr]
 end)()
 
 local log = args.verbose and print or function() end
 
 require('lfs').mkdir(args.cache)
 
-local casc = (function()
+local cascs = (function()
+  local products = {
+    'wow',
+    'wowt',
+    'wow_classic',
+    'wow_classic_era',
+    'wow_classic_era_ptr',
+    'wow_classic_ptr',
+  }
   local casc = require('casc')
-  local url = 'http://us.patch.battle.net:1119/' .. product
-  local bkey, cdn, ckey, version = casc.cdnbuild(url, 'us')
-  if not bkey then
-    print('unable to open ' .. product .. ': cannot get version')
-    os.exit()
+  local cascs = {}
+  for _, product in ipairs(products) do
+    local url = 'http://us.patch.battle.net:1119/' .. product
+    local bkey, cdn, ckey, version = casc.cdnbuild(url, 'us')
+    if not bkey then
+      print('unable to open ' .. product .. ': cannot get version')
+      os.exit()
+    end
+    log('loading', version, url)
+    local handle, err = casc.open({
+      bkey = bkey,
+      cache = args.cache,
+      cacheFiles = true,
+      cdn = cdn,
+      ckey = ckey,
+      locale = casc.locale.US,
+      log = log,
+      zerofillEncryptedChunks = true,
+    })
+    if not handle then
+      print('unable to open ' .. product .. ': ' .. err)
+      os.exit()
+    end
+    cascs[product] = handle
   end
-  log('loading', version, url)
-  local handle, err = casc.open({
-    bkey = bkey,
-    cache = 'cache',
-    cacheFiles = true,
-    cdn = cdn,
-    ckey = ckey,
-    locale = casc.locale.US,
-    log = log,
-    zerofillEncryptedChunks = true,
-  })
-  if not handle then
-    print('unable to open ' .. product .. ': ' .. err)
-    os.exit()
-  end
-  return handle
+  return cascs
 end)()
+
+collectgarbage()
+collectgarbage()
 
 local pathparser = (function()
   local lpeg = require('lpeg')
@@ -67,9 +71,10 @@ local listener = assert(require('http.server').listen({
   end,
   onstream = function(_, stream)
     local req = assert(stream:get_headers())
-    local tag, fid = pathparser:match(req:get(':path'))
+    local product, fid = pathparser:match(req:get(':path'))
+    local casc = cascs[product]
     local res = mkres()
-    if not tag or not fid or tag ~= product then
+    if not casc or not fid then
       res:append(':status', '400')
       assert(stream:write_headers(res, true))
       return
@@ -86,6 +91,7 @@ local listener = assert(require('http.server').listen({
   end,
   port = args.port,
 }))
+log('HTTP server ready')
 pcall(function()
   listener:loop()
 end)
